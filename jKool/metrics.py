5be5db9c9cfabc4f7c14050d371758e0448dfbbb
Albert <albertm@jkoolcloud.com>
2016-07-19 13:34:11 -0400

    copyright header

--- jKool/metrics.py
+++ jKool/metrics.py
@@ -1,3 +1,19 @@
+# 
+#  Copyright 2015 JKOOL, LLC.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 class Property:
     """Simple model for user defined properties. Holds a name, value, and type of the property."""
     def __init__(self, name, value, property_type):
