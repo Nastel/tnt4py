f19ddde58d410db906f916ae1196909a1118edd7
dschling <d.schling@yahoo.com>
2016-07-27 18:33:56 -0400

    Added MqttHandler
    Not yet authorization for MqttHandler. Class jKoolHandler now takes a
    protocol parameter in constructor to determine to use HttpHandler or
    MqttHandler.

--- jKool/streaming.py
+++ jKool/streaming.py
@@ -43,118 +43,168 @@ class AuthorizationError(Exception):
         return repr(self.value)
 
     
-    
-    
 class jKoolHandler(logging.Handler):
-    """Logging handler that will stream to jKool cloud service."""
-        
-    def __init__(self, accessToken, urlStr="https://data.jkoolcloud.com", level=logging.INFO):
-        logging.Handler.__init__(self, level)
-        
-        self.level = level
-        self.token = accessToken
-            
-        self.url = urlStr
-        uri = urlparse(self.url)
-        
-        scheme = uri.scheme
-        self.secure = ("https" == scheme)
-        
-        self.host = uri.hostname
-        self.path = uri.path
-        self.port = uri.port
-        
-        if self.host == None:
-            self.host = "localhost"
-            
-        self.connect()
-        
-            
-    def emit(self, record):
+    """Logging handler to stream to jKool cloud service using specified protocol."""
+    
+    def logRecordToJsonString(record):
         name = record.name
         time = int(record.created) * 1000000
         message = record.getMessage()
         level = record.levelname
-        
-        extras = record.__dict__
-        tagNames = extras['allTags']
-        tags = {}
-        for tag in tagNames:
-            jsonTag = tag.replace('_', '-')
-            tags[jsonTag] = extras[tag]
-            
-        
         payload={'operation': name, 'type':'EVENT', 'time-usec':time, 'msg-text':message, 'severity':level}
-        payload.update(tags)
-        formatted = json.dumps(payload, cls = SnapshotEncoder)
-        headers = {"Content-Type": "application/json"}
-        
-        
-        try:
-            self.connection.request("POST", self.path, formatted, headers)
-        except (ConnectionError, HTTPException, timeout) as err:
-            conn.close()
-            raise err
-        else:
-            response = self.connection.getresponse()
-            data = response.read()
-            print(response.status, response.reason)
 
-            
+
+        extras = record.__dict__
+        try:
+            tagNames = extras['allTags']
+            tags = {}
+            for tag in tagNames:
+                jsonTag = tag.replace('_', '-')
+                tags[jsonTag] = extras[tag]
+            payload.update(tags)
+        except KeyError:
+            pass
+
+        return json.dumps(payload, cls = SnapshotEncoder)
+    
         
-        
-    def connect(self):
-        if self.secure:
+    class HttpHandler():
+        """Logging handler that will stream to jKool cloud service using http/s."""
+
+        def __init__(self, accessToken, urlStr="https://data.jkoolcloud.com", level=logging.INFO):
+
+            self.level = level
+            self.token = accessToken
+
+            self.url = urlStr
+            uri = urlparse(self.url)
+
+            scheme = uri.scheme
+            self.secure = ("https" == scheme)
+
+            self.host = uri.hostname
+            self.path = uri.path
+            self.port = uri.port
+
+            if self.host == None:
+                self.host = "localhost"
+
+            self.connect()
+
+
+        def emit(self, record):
             
-            if self.port != None:
-                conn = http.client.HTTPSConnection(self.host, port=self.port, timeout=10)
+            message = jKoolHandler.logRecordToJsonString(record)
+            headers = {"Content-Type": "application/json"}
+
+            try:
+                self.connection.request("POST", self.path, formatted, headers)
+            except (ConnectionError, HTTPException, timeout) as err:
+                conn.close()
+                raise err
             else:
-                conn = http.client.HTTPSConnection(self.host, timeout=10)
-                
-        else:
+                response = self.connection.getresponse()
+                data = response.read()
+                print(response.status, response.reason)
+
+
+        def connect(self):
             
-            if self.port != None:
-                conn = http.client.HTTPConnection(self.host, port=self.port, timeout=10)
+            if self.secure:
+                if self.port != None:
+                    conn = http.client.HTTPSConnection(self.host, port=self.port, timeout=10)
+                else:
+                    conn = http.client.HTTPSConnection(self.host, timeout=10)
             else:
-                conn = http.client.HTTPConnection(self.host, timeout=10)
-        
-        try:
-            conn.connect()
-        except (ConnectionError, HTTPException) as err:
-            conn.close()
-            raise err
-        else:
-            print("Connected")
-            self.connection = conn
-            
-        self.sendAuthRequest(self.connection)
-        
-    def sendAuthRequest(self, conn):
-        """Attempts to authorize the token with jKool"""
-        
-        msg = "<access-request><token>" + self.token + "</token></access-request>"
-        headers = {"Content-Type": "text/plain"}
-        
-        try:
-            conn.request("POST", self.path, msg, headers)
-        except (ConnectionError, HTTPException) as err:
-            conn.close()
-            raise err
-        else:
-            response = conn.getresponse()
-            data = response.read()
-        
-        if response.status >= 200 and response.status < 300:
-            print("Authorized")
-        else:
-            conn.close()
-            print("response.status, response.reason")
-            raise AuthorizationError("Error authorizing token")
+                if self.port != None:
+                    conn = http.client.HTTPConnection(self.host, port=self.port, timeout=10)
+                else:
+                    conn = http.client.HTTPConnection(self.host, timeout=10)
 
+            try:
+                conn.connect()
+            except (ConnectionError, HTTPException) as err:
+                conn.close()
+                raise err
+            else:
+                print("Connected")
+                self.connection = conn
 
-            
+            self.sendAuthRequest(self.connection)
+
+        def sendAuthRequest(self, conn):
+            """Attempts to authorize the token with jKool"""
+
+            msg = "<access-request><token>" + self.token + "</token></access-request>"
+            headers = {"Content-Type": "text/plain"}
+
+            try:
+                conn.request("POST", self.path, msg, headers)
+            except (ConnectionError, HTTPException) as err:
+                conn.close()
+                raise err
+            else:
+                response = conn.getresponse()
+                data = response.read()
+
+            if response.status >= 200 and response.status < 300:
+                print("Authorized")
+            else:
+                conn.close()
+                print("response.status, response.reason")
+                raise AuthorizationError("Error authorizing token")
 
     
+    class MqttHandler:
+        """Logging handler that streams to jKool using mqtt"""
+        
+        def __init__(self, accessToken, urlStr, level=logging.INFO, keepalive=60):
+            import paho.mqtt.client as mqtt
+
+            self.url = urlStr
+            self.port = urlparse(urlStr).port
+            
+            self.keepalive = keepalive
+            
+            self.client = mqtt.Client()
+            
+            self.connect()
+            
+            
+        def connect(self):
+            """Connect client to broker and start network loop"""
+            if self.port == None:
+                self.client.connect(self.url, keepalive=self.keepalive)
+            else:
+                self.client.connect(self.url, self.port, self.keepalive)
+                
+            self.client.loop_start()
+            
+            
+        def emit(self, record):
+            topic = record.name
+            message = jKoolHandler.logRecordToJsonString(record)
+            print(message)
+            
+            result, mid = self.client.publish(topic, message)
+            print(result, mid)
+    
+    
+    def __init__(self, accessToken, urlStr="https://data.jkoolcloud.com", level=logging.INFO, protocol="http"):
+        logging.Handler.__init__(self, level)
+
+        if protocol == "http":
+            self.handler = self.HttpHandler(accessToken, urlStr, level)
+        elif protocol == "mqtt":
+            self.handler = self.MqttHandler(accessToken, urlStr, level)
+        else:
+            raise ValueError("Invalid protocol")
+            
+    
+    def emit(self, record):
+        self.handler.emit(record)
+
+
     
 def logEvent(logger, msg_text, source_fqn, tracking_id=str(uuid.uuid4()), time_usec=None, corr_id=None, 
             exception=None, resource=None, wait_time_used=None, source_url=None,
@@ -163,6 +213,7 @@ def logEvent(logger, msg_text, source_fqn, tracking_id=str(uuid.uuid4()), time_u
             elapsed_time_usec=None, msg_size=None, encoding=None,
             charset=None, mime_type=None, msg_age=None, msg_tag=None, parent_tracking_id=None,
             properties = None, snapshots = None):
+    """Helper method to enrich event streaming"""
     
     arguments = locals()
     emptyKeys = [k for k, v in arguments.items() if v is None or k == 'logger']
